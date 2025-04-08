9cb236060ef4ccfe2e263501ff305507e4392c4d
emi asahida <liuli.hudie@gmail.com>
2025-04-09 02:36:18 +0900

    初回コミット

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+const GAS_URL = 'https://script.google.com/macros/s/XXXXXX/exec';
+
+app.post('/webhook', async (req, res) => {
+  try {
+    const response = await axios.post(GAS_URL, req.body);
+    res.status(200).send('OK');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error forwarding to GAS');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
